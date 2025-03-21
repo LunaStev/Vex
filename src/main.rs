1b1cb7b0c1b196faa934a2458737ad7e56dbc6d4
LunaStev <youngjae681@gmail.com>
2025-03-21 19:53:58 +0900

    main

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use crate::init::run;
+
+mod init;
+mod run;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let is_lib = args.iter().any(|arg| arg == "--lib");
+
+    if args.len() >= 2 && args[1] == "init" {
+        run(is_lib);
+    } else {
+        println!("❌ Unknown command.");
+    }
+}
