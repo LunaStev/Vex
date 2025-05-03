47df8fe070cafd525096019a601207802e4d4989
LunaStev <youngjae681@gmail.com>
2025-05-03 18:59:01 +0900

    Version Command

---
+++ src/version.rs
@@ -0,0 +1,13 @@
+use colorex::Colorize;
+use wavec::version;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn version() -> &'static str {
+    let version = VERSION;
+    version
+}
+
+pub fn version_vex() {
+    println!("{} {}", "vex".color("2,161,47"), version().color("2,161,47"));
+}
